a8bc58693c39edcccdbb844ef09e673ad3d96c08
by22dgb <by22dgb@gmail.com>
2021-06-08 12:28:49 +0800

    更新3.3.1.61

--- scripts/evolve.js
+++ scripts/evolve.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Evolve
 // @namespace    http://tampermonkey.net/
-// @version      3.3.1.59
+// @version      3.3.1.61
 // @description  try to take over the world!
 // @downloadURL  https://gitee.com/likexia/Evolve/raw/master/scripts/evolve.js
 // @author       Fafnir
@@ -734,12 +734,13 @@
         }
 
         get title() {
-            if (this.definition !== undefined) {
-                return typeof this.definition.title === 'string' ? this.definition.title : this.definition.title();
-            }
+            let def = this.definition;
+            return def ? typeof def.title === 'string' ? def.title : def.title() : this.name;
+        }
 
-            // There is no definition...
-            return this.name;
+        get desc() {
+            let def = this.definition;
+            return def ? typeof def.desc === 'string' ? def.desc : def.desc() : this.name;
         }
 
         get vue() {
@@ -2338,7 +2339,7 @@
           () => "No more Horseshoes needed",
           () => settings.buildingWeightingHorseshoeUseless
       ],[
-          () => game.global.race.calm && resources.Zen.storageRatio < 1,
+          () => game.global.race.calm && resources.Zen.currentQuantity < resources.Zen.maxQuantity,
           (building) => building === buildings.MeditationChamber,
           () => "No more Meditation Space needed",
           () => settings.buildingWeightingZenUseless
@@ -4741,7 +4742,6 @@
         JobManager.addJobToPriorityList(jobs.Hunter);
         JobManager.addJobToPriorityList(jobs.Farmer);
         //JobManager.addJobToPriorityList(jobs.Forager);
-        JobManager.addJobToPriorityList(jobs.Miner);
         JobManager.addJobToPriorityList(jobs.Lumberjack);
         JobManager.addJobToPriorityList(jobs.QuarryWorker);
         JobManager.addJobToPriorityList(jobs.CrystalMiner);
@@ -4754,6 +4754,7 @@
         JobManager.addJobToPriorityList(jobs.HellSurveyor);
         JobManager.addJobToPriorityList(jobs.SpaceMiner);
         JobManager.addJobToPriorityList(jobs.Archaeologist);
+        JobManager.addJobToPriorityList(jobs.Miner);
         JobManager.addJobToPriorityList(jobs.CoalMiner);
         JobManager.addJobToPriorityList(jobs.Banker);
         JobManager.addJobToPriorityList(jobs.Priest);
@@ -6070,10 +6071,12 @@
 
         // Appoint governor
         if (haveTech("governor") && settings.govGovernor !== "none" && getGovernor() === "") {
-            let govId = game.global.race.governor?.candidates?.findIndex(governor => governor.bg === settings.govGovernor);
-            let vue = getVueById("candidates");
-            if (vue && govId >= 0) {
-                vue.appoint(govId);
+            let candidates = game.global.race.governor?.candidates ?? [];
+            for (let i = 0; i < candidates.length; i++) {
+                if (candidates[i].bg === settings.govGovernor) {
+                    getVueById("candidates")?.appoint(i);
+                    break;
+                }
             }
         }
     }
@@ -8714,6 +8717,7 @@
         let importRouteCap = MarketManager.getImportRouteCap();
         let exportRouteCap = MarketManager.getExportRouteCap();
 
+        // TODO: Sell excess
         // Fill trade routes with selling
         for (let i = 0; i < tradableResources.length; i++) {
             let resource = tradableResources[i];
@@ -9778,20 +9782,19 @@
             return;
         }
         mutations.forEach(mutation => mutation.addedNodes.forEach(node => {
-            if (node.childElementCount === 0) { // Descriptions tooltip
-                return;
+            if (node.innerHTML.startsWith('<div>')) {
+                for (let building of Object.values(buildings)){
+                    if (node.innerHTML.startsWith('<div>' + building.desc)){
+                        node.innerHTML += `<div>${building.extraDescription}</div>`;
+                        return;
+                    }
+                }
             }
-            let obj = null;
-            if (node.id.match(/^poppopArpa/)) { // "poppopArpa[id-with-no-tab]" for projects
-                obj = arpaIds["arpa" + node.id.substr(10)];
-            } else if (node.id.match(/\d$/)) { // "popq[id][order]" for buildings in queue
-                let id = node.id.substr(4, node.id.length-5);
-                obj = buildingIds[id] || arpaIds[id];
-            } else { // "pop[id]" for normal buildings
-                obj = buildingIds[node.id.substr(3)];
-            }
-            if (obj && obj.extraDescription !== "") {
-                node.innerHTML += `<div>${obj.extraDescription}</div>`;
+            for (let project of Object.values(projects)){
+                if (node.innerHTML.startsWith(project.desc)){
+                    node.innerHTML += `<div style="border-top: solid .0625rem #999">${project.extraDescription}</div>`;
+                    return;
+                }
             }
         }));
     }
@@ -13263,7 +13266,7 @@
                 if (oldStats.sac > 0) {
                     statsData.sacrificed = oldStats.sac;
                 }
-                let statsString = `<div class="cstat"><span class="has-text-success">Previous Game</span></div>`;
+                let statsString = `<div class="cstat"><span class="has-text-success">上周目数据</span></div>`;
                 for (let [label, value] of Object.entries(statsData)) {
                     statsString += `<div><span class="has-text-warning">${game.loc("achieve_stats_" + label)}</span> ${value.toLocaleString()}</div>`;
                 }
